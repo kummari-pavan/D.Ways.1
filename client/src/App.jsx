43151f4e4a08b3592c17cb7a2eee27b8f564ed73
kummari-pavan <kpavan892002@gmail.com>
2024-12-01 16:51:03 +0530

    [PAVAN][client]:Added Account checkout home listing pages in shoping view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,20 +7,32 @@ import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
 import AdminLayout from './components/admin-view/layout'
+import AdminDashboard from './pages/admin-view/dashboard'
+import AdminProducts from './pages/admin-view/products'
+import AdminOrders from './pages/admin-view/orders'
+import AdminFeatures from './pages/admin-view/features'
+import ShoppingLayout from './components/shopping-view/layout'
+import NotFound from './pages/not-found'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="flex flex-col overflow-hidden bg-white">
-      <h1>Header Component</h1>
          <Routes>
-          <Route path='/auth' element={<AuthLayout/>}>
-            <Route path="login" element={<AuthLogin />} />
-            <Route path="register" element={<AuthRegister />} />
+           <Route path='/auth' element={<AuthLayout/>}>
+              <Route path="login" element={<AuthLogin />} />
+              <Route path="register" element={<AuthRegister />} />
             </Route>
-            <Route path="admin" element={<AdminLayout/>} />
-            
+            <Route path="/admin" element={<AdminLayout/>}>
+              <Route path="dashboard" element={<AdminDashboard/>} />
+              <Route path="products" element={<AdminProducts/>} />
+              <Route path="orders" element={<AdminOrders/>} />
+              <Route path="features" element={<AdminFeatures/>}/>
+            </Route>
+            <Route path="/shop" element={<ShoppingLayout/>}>
+              <Route path='*' element={<NotFound/>}/>
+            </Route>
          </Routes>
 
     </div>
