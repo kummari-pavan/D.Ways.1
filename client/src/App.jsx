de3a52ea64713c1bc99a3a6125419c2d1f517582
kummari-pavan <kpavan892002@gmail.com>
2024-12-01 17:02:00 +0530

    [PAVAN][client]:Added Routes for Auth ,admin , shooping along with their child components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,10 @@ import AdminOrders from './pages/admin-view/orders'
 import AdminFeatures from './pages/admin-view/features'
 import ShoppingLayout from './components/shopping-view/layout'
 import NotFound from './pages/not-found'
+import ShoppingHome from './pages/shopping-view/home'
+import ShoppingListing from './pages/shopping-view/listing'
+import ShoppingCheckout from './pages/shopping-view/checkout'
+import ShoppingAccount from './pages/shopping-view/account'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,8 +35,12 @@ function App() {
               <Route path="features" element={<AdminFeatures/>}/>
             </Route>
             <Route path="/shop" element={<ShoppingLayout/>}>
-              <Route path='*' element={<NotFound/>}/>
+              <Route path="home" element={<ShoppingHome/>}/>
+              <Route path="listing" element={<ShoppingListing/>}/>
+              <Route path="checkout" element={<ShoppingCheckout/>}/>
+              <Route path="account" element={<ShoppingAccount/>}/>
             </Route>
+            <Route path='*' element={<NotFound/>}/>
          </Routes>
 
     </div>
