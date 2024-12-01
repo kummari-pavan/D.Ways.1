0a35dc9074558b86464345857f1456359667e2b7
kummari-pavan <kpavan892002@gmail.com>
2024-12-01 13:46:16 +0530

    [PAVAN][client]:setup vit and tailwind CSS

--- server-pavan/server.js
+++ server-pavan/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 
 
@@ -14,3 +16,23 @@ mongoose
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST", "DELETE", "PUT"],
+      allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Cache-Control",
+        "Expires",
+        "Pragma",
+      ],
+      credentials: true,
+    })
+  );
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
