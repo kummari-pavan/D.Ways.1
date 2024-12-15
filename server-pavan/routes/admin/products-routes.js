3fb70445cafe9a51e52dd0e4c2c426b44a8c93b0
kummari-pavan <kpavan892002@gmail.com>
2024-12-16 00:16:42 +0530

    [PAVAN][Server-Pavan]: Routes : for Products

--- server-pavan/routes/admin/products-routes.js
+++ server-pavan/routes/admin/products-routes.js
@@ -9,7 +9,6 @@ const {
 } = require("../../controllers/admin/products-controller");
 
 const { upload } = require("../../helpers/cloudinary");
-
 const router = express.Router();
 
 router.post("/upload-image", upload.single("my_file"), handleImageUpload);
