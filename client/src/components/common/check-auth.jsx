41e9dcf9023c67936df8c4034e7abeb7a9d8c1ac
kummari-pavan <kpavan892002@gmail.com>
2024-12-15 12:28:24 +0530

    [PAVAN][Cient]: components : star-rating : check-auth,form,star-rating

--- client/src/components/common/check-auth.jsx
+++ client/src/components/common/check-auth.jsx
@@ -1,47 +1,109 @@
-import { Navigate } from "react-router-dom"
+// import { Navigate } from "react-router-dom"
 
-function CheckAuth(isAuthenticated,user,children){
-    const location =useLocation()
-    if (
-        !isAuthenticated &&
-        !(
-          location.pathname.includes("/login") ||
-          location.pathname.includes("/register")
-        )
-      ) {
-        return <Navigate to="/auth/login" />;
-      }
+// function CheckAuth(isAuthenticated,user,children){
+//     const location =useLocation()
+//     if (
+//         !isAuthenticated &&
+//         !(
+//           location.pathname.includes("/login") ||
+//           location.pathname.includes("/register")
+//         )
+//       ) {
+//         return <Navigate to="/auth/login" />;
+//       }
     
-      if (
-        isAuthenticated &&
-        (location.pathname.includes("/login") ||
-          location.pathname.includes("/register"))
-      ) {
-        if (user?.role === "admin") {
-          return <Navigate to="/admin/dashboard" />;
-        } else {
-          return <Navigate to="/shop/home" />;
-        }
-      }
+//       if (
+//         isAuthenticated &&
+//         (location.pathname.includes("/login") ||
+//           location.pathname.includes("/register"))
+//       ) {
+//         if (user?.role === "admin") {
+//           return <Navigate to="/admin/dashboard" />;
+//         } else {
+//           return <Navigate to="/shop/home" />;
+//         }
+//       }
     
-      if (
-        isAuthenticated &&
-        user?.role !== "admin" &&
-        location.pathname.includes("admin")
-      ) {
-        return <Navigate to="/unauth-page" />;
-      }
+//       if (
+//         isAuthenticated &&
+//         user?.role !== "admin" &&
+//         location.pathname.includes("admin")
+//       ) {
+//         return <Navigate to="/unauth-page" />;
+//       }
     
-      if (
-        isAuthenticated &&
-        user?.role === "admin" &&
-        location.pathname.includes("shop")
-      ) {
+//       if (
+//         isAuthenticated &&
+//         user?.role === "admin" &&
+//         location.pathname.includes("shop")
+//       ) {
+//         return <Navigate to="/admin/dashboard" />;
+//       }
+    
+//       return <>{children}</>;
+    
+// }
+
+// export default CheckAuth
+
+import { Navigate, useLocation } from "react-router-dom";
+
+function CheckAuth({ isAuthenticated, user, children }) {
+  const location = useLocation();
+
+  console.log(location.pathname, isAuthenticated);
+
+  if (location.pathname === "/") {
+    if (!isAuthenticated) {
+      return <Navigate to="/auth/login" />;
+    } else {
+      if (user?.role === "admin") {
         return <Navigate to="/admin/dashboard" />;
+      } else {
+        return <Navigate to="/shop/home" />;
       }
-    
-      return <>{children}</>;
-    
+    }
+  }
+
+  if (
+    !isAuthenticated &&
+    !(
+      location.pathname.includes("/login") ||
+      location.pathname.includes("/register")
+    )
+  ) {
+    return <Navigate to="/auth/login" />;
+  }
+
+  if (
+    isAuthenticated &&
+    (location.pathname.includes("/login") ||
+      location.pathname.includes("/register"))
+  ) {
+    if (user?.role === "admin") {
+      return <Navigate to="/admin/dashboard" />;
+    } else {
+      return <Navigate to="/shop/home" />;
+    }
+  }
+
+  if (
+    isAuthenticated &&
+    user?.role !== "admin" &&
+    location.pathname.includes("admin")
+  ) {
+    return <Navigate to="/unauth-page" />;
+  }
+
+  if (
+    isAuthenticated &&
+    user?.role === "admin" &&
+    location.pathname.includes("shop")
+  ) {
+    return <Navigate to="/admin/dashboard" />;
+  }
+
+  return <>{children}</>;
 }
 
-export default CheckAuth
+export default CheckAuth;
